de4a21fe9f3dfd40a8f5fe1d94b742986cee8324
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-06-15 16:36:00 -0500

    Reducer

---
+++ src/redux/reducer.js
@@ -0,0 +1,16 @@
+import { ADD_ONE, MINUS_ONE } from './actions';
+
+const initialState = {
+  counter: 0,
+};
+
+export default function reducer(state = initialState, action) {
+   switch (action.type) {
+     case ADD_ONE:
+       return {counter: state.counter + 1 };
+     case MINUS_ONE:
+      return {counter: state.counter - 1 };
+     default:
+       return state;
+ }
+}
